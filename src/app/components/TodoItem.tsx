7e98ea8b564fccd8c107073a529c06ce3a4c84b8
Kazuya Yokoyama <yk3kzy@gmail.com>
2023-11-11 08:17:38 +0900

    TodoItem を削除できる

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { useRecoilState } from 'recoil'
+import { todoListState } from '../data-flow/todoList'
 import { TodoListItem } from '../types/TodoListItem'
 
 type TodoItemProps = {
@@ -5,5 +7,19 @@ type TodoItemProps = {
 }
 
 export const TodoItem = ({ item }: TodoItemProps) => {
-  return <div>{item.title}</div>
+  const [todoList, setTodoList] = useRecoilState(todoListState)
+
+  const deleteItem = () => {
+    const newTodoList = todoList.filter((listItem) => listItem.id !== item.id)
+    setTodoList(newTodoList)
+  }
+
+  return (
+    <div>
+      {item.title}
+      <span onClick={deleteItem} style={{ cursor: 'pointer' }}>
+        X
+      </span>
+    </div>
+  )
 }
